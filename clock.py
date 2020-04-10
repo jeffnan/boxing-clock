eb31b49cc91f237347b38eb21fca3b7f2f89a8a3
jeffnan <jeffnan@umich.edu>
2020-04-10 16:01:47 -0400

    Adjusted volume and alarm sound

--- clock.py
+++ clock.py
@@ -4,6 +4,7 @@ import os
 import subprocess
 import applescript
 
+#Update with new GUI
 #Should this be dynamic for any music player?
 #Should I build function for the ringing?
 #Include aiff in the git folder
@@ -14,24 +15,25 @@ playPauseScript = """tell app "Spotify" to playpause"""
 currentMusicStateScript = """tell app "Spotify" to get player state"""
 
 currentVolScript = """output volume of (get volume settings)"""
-currentVol = int(subprocess.run(['osascript', '-e', currentVolScript], capture_output = True).stdout.decode('UTF-8'))
+maxVolScript = """set volume output volume 100"""
 
-maxVolScript = """set volume output volume 40"""
-normVolScript = """set volume output volume """ + str(currentVol)
 
 def roundChangeAlarm(rings: int):
 	"""Pauses music if music is playing, changes to max volume to ring alarm, sets volume back to normal and continues music"""
+	currentVol = int(subprocess.run(['osascript', '-e', currentVolScript], capture_output = True).stdout.decode('UTF-8'))
+	normVolScript = """set volume output volume """ + str(currentVol)
 	currentMusicState = subprocess.run(['osascript', '-e', currentMusicStateScript], capture_output = True).stdout.decode('UTF-8').strip()
+
 	if currentMusicState == 'paused':
 		subprocess.call(['osascript', '-e', maxVolScript])
 		for _ in range(rings):
-		    os.system('afplay -r 3 /System/Library/Sounds/Glass.aiff')
+		    os.system('afplay /System/Library/Sounds/Blow.aiff')
 		subprocess.call(['osascript', '-e', normVolScript])
 	else:
 		subprocess.call(['osascript', '-e', playPauseScript])
 		subprocess.call(['osascript', '-e', maxVolScript])
 		for _ in range(rings):
-		    os.system('afplay -r 3 /System/Library/Sounds/Glass.aiff')
+		    os.system('afplay /System/Library/Sounds/Blow.aiff')
 		subprocess.call(['osascript', '-e', normVolScript])
 		subprocess.call(['osascript', '-e', playPauseScript])
 
@@ -53,6 +55,8 @@ t1 = Turtle()
 
 t1Screen = Screen()
 t1.hideturtle()
+t1Screen.setup(width=1.0, height=1.0)
+
 minutesBoxing = int(t1Screen.numinput(title = "Boxing Round", prompt = "How many minutes would you like your boxing round to be?", default = None, minval = 0, maxval = 5))
 secondsBoxing = int(t1Screen.numinput(title = "Boxing Round", prompt = "How many seconds would you like your boxing round to be?", default = None, minval = 0, maxval = 60))
 
@@ -83,6 +87,9 @@ for _ in range(numOfRounds):
 
 	screenDisplay(red, minutesRest, secondsRest, restRound)
 	roundChangeAlarm(3)
+
+#GitHub
+#https://towardsdatascience.com/getting-started-with-git-and-github-6fcd0f2d4ac6
 
 
 # AppleScript
